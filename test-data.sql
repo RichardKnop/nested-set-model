6cec277ceb90137e674060c13bd854fd6201f6f9
Richard Knop <richard.knop@saffrondigital.com>
2012-09-25 22:14:45 +0100

    initial commit, added schema sql and test data sql

---
+++ test-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (id, name, lft, rgt) VALUES
+(NULL, 'Relational databases', 1, 12),
+(NULL, 'SQLite', 2, 3),
+(NULL, 'MySQL', 4, 9),
+(NULL, 'PDO', 5, 6),
+(NULL, 'Transactions', 7, 8),
+(NULL, 'PostgreSQL', 10, 11);
